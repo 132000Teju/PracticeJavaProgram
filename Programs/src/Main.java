911a5d24d60fc4335904e7a0a3d4f0d0275bb0cc
132000Teju <tejaswinipotdar13@gmail.com>
2024-05-19 15:02:08 +0530

    first commit

---
+++ Programs/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'A';
+        System.out.println(myChar);
+        char myChar1 = '1';
+        System.out.println(myChar1);
+
+        char myuniChar1 = '\u013f';
+        System.out.println(myuniChar1);
+    }
+}
